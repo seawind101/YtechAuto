c736e78a46b045320f90539a58465266475de130
Kayden <27KlaRosa@live.ytech.edu>
2026-03-18 08:42:15 -0400

    day 3 on the job

--- app.js
+++ app.js
@@ -2,22 +2,37 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
 const jwt = require('jsonwebtoken');
 const session = require('express-session');
 const multer = require('multer');
 const PORT = process.env.PORT || 3000;
 const http = require('http');
 const server = require('http').createServer(app);
+const sqlite3 = require('sqlite3');
+const fs = require('fs');
 //middelware
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'public', 'views'));
 
 const db = new sqlite3.Database('database/database.sql', (err) => {
     if (err) return console.error('Error connecting to database:', err.message);
+    //you can run database migrations in here 
 });
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.get('/customer', (req, res) => {
+    res.render('customer');
+});
+
+app.get('/mechanic', (req, res) => {
+    res.render('mechanic');
+});
+
 server.listen(PORT, () => {
     console.log(`Example app listening on port http://localhost:${PORT}`);
 });

277c7df0466a64550f01693d3e655bc0bf67d85d
Markus Owens <167331008+27MOwens@users.noreply.github.com>
2026-03-18 05:24:25 -0700

    added routes to app.js and fixed links

--- app.js
+++ app.js
@@ -10,10 +10,17 @@ const http = require('http');
 const server = require('http').createServer(app);
 //middelware
 app.set('view engine', 'ejs');
-
-
+//index route
 app.get('/', (req, res) => {
   res.render('index');
+});
+//customer route
+app.get('/customer', (req, res) => {
+  res.render('customer');
+});
+//mechanic route
+app.get('/mechanic', (req, res) => {
+  res.render('mechanic');
 });
 server.listen(PORT, () => {
     console.log(`Example app listening on port http://localhost:${PORT}`);
